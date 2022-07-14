f73387b825059aa792fe8daf6e7b145d27d1a07a
MNewman <megan.m.boggs@gmail.com>
2022-07-13 23:24:32 -0700

    setup conditionals for rps game

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -5,17 +5,33 @@
 // declare a winner using conditionals
 // create a loop for other games
 
-function computerPlay() {
-  let options = ["rock", "paper", "scissors"];
-  return variables[Math.floor(Math.random() * options.length)];
-}
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    function playRound() {
-      let playerSelection = "rock";
-      let computerSelection = computerPlay();
-      playRound(playerSelection, computerSelection);
-    }
+let playRound = function (playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if (playerSelection === "Paper" && computerSelection === "scissors") {
+    return "-1. Paper < Scissors";
+  } else if (playerSelection === "Paper" && computerSelection === "rock") {
+    return "+1. Paper > Rock";
+  } else if (playerSelection === "Scissors" && computerSelection === "paper") {
+    return "+1. Scissors > Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "rock") {
+    return "-1. Scissors < Rock";
+  } else if (playerSelection === "Rock" && computerSelection === "paper") {
+    return "-1. Rock < Paper";
+  } else if (playerSelection === "Rock" && computerSelection === "scissors") {
+    return "+1. Rock > Scissors.";
+  } else {
+    return "not a valid option. please choose again.";
   }
-}
+};
+
+let computerPlay = function () {
+  let options = ["rock", "paper", "scissors"];
+
+  let randomOption = options[Math.floor(Math.random() * options.length)];
+  return `${randomOption}`;
+};
+
+const playerSelection = prompt("Choose wisely - Rock, Paper, or Scissors?");
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
