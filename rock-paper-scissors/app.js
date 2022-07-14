cd7dd4c572a8b1310cb5b260943854caaa05b3ef
MNewman <megan.m.boggs@gmail.com>
2022-07-14 00:06:04 -0700

    updated game function for player scores

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -5,31 +5,54 @@
 // declare a winner using conditionals
 // create a loop for other games
 
-let playRound = function (playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "Tie!";
-  } else if (playerSelection === "Paper" && computerSelection === "scissors") {
-    return "-1. Paper < Scissors";
-  } else if (playerSelection === "Paper" && computerSelection === "rock") {
-    return "+1. Paper > Rock";
-  } else if (playerSelection === "Scissors" && computerSelection === "paper") {
-    return "+1. Scissors > Paper";
-  } else if (playerSelection === "Scissors" && computerSelection === "rock") {
-    return "-1. Scissors < Rock";
-  } else if (playerSelection === "Rock" && computerSelection === "paper") {
-    return "-1. Rock < Paper";
-  } else if (playerSelection === "Rock" && computerSelection === "scissors") {
-    return "+1. Rock > Scissors.";
-  } else {
-    return "not a valid option. please choose again.";
-  }
-};
+let playerScore = 0;
+let computerScore = 0;
 
 let computerPlay = function () {
   let options = ["rock", "paper", "scissors"];
-
   let randomOption = options[Math.floor(Math.random() * options.length)];
   return `${randomOption}`;
+};
+
+let playRound = function (playerSelection, computerSelection) {
+  for (let i = 1; i <= 5; i++)
+    if (playerScore < 5 && computerScore < 5) {
+      if (playerSelection === computerSelection) {
+        return "Tie!";
+      } else if (
+        playerSelection === "Paper" &&
+        computerSelection === "scissors"
+      ) {
+        computerScore++;
+        return "-1. Paper < Scissors";
+      } else if (playerSelection === "Paper" && computerSelection === "rock") {
+        playerScore++;
+        return "+1. Paper > Rock";
+      } else if (
+        playerSelection === "Scissors" &&
+        computerSelection === "paper"
+      ) {
+        playerScore++;
+        return "+1. Scissors > Paper";
+      } else if (
+        playerSelection === "Scissors" &&
+        computerSelection === "rock"
+      ) {
+        computerScore++;
+        return "-1. Scissors < Rock";
+      } else if (playerSelection === "Rock" && computerSelection === "paper") {
+        computerScore++;
+        return "-1. Rock < Paper";
+      } else if (
+        playerSelection === "Rock" &&
+        computerSelection === "scissors"
+      ) {
+        playerScore++;
+        return "+1. Rock > Scissors.";
+      } else {
+        return "not a valid option. please choose again.";
+      }
+    }
 };
 
 const playerSelection = prompt("Choose wisely - Rock, Paper, or Scissors?");
